5a9247da5d52605dfec33e11345f52b6e1e863cc
Norbey <wilson.norbey@gisoft.co>
2023-07-20 10:53:27 -0500

    feat:se activa Swagger y descarga de videos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,42 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { VideoController } from './video.controller';
+import * as express from 'express';
+import { INestApplication } from '@nestjs/common';
 
 @Module({
-  imports: [AuthModule, UsersModule],
-  controllers: [AppController],
+  imports: [
+    AuthModule,
+    UsersModule,
+    MulterModule.register({
+      dest: './uploads', // Directorio donde se guardarán los videos
+    }),
+  ],
+  controllers: [VideoController, AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+async function bootstrap() {
+  const server = express();
+  const app = await NestFactory.create<INestApplication>(AppModule, new ExpressAdapter(server));
+
+  const options = new DocumentBuilder()
+    .setTitle('Nombre de tu API')
+    .setDescription('Descripción de tu API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+
+  // Establece la ruta para servir la documentación Swagger en '/api'
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+
+bootstrap();
---
+++ src/video.controller.ts
@@ -0,0 +1,41 @@
+import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Response } from 'express';
+import * as ytdl from 'ytdl-core';
+
+@Controller('videos')
+export class VideoController {
+  @Get(':videoLink')
+  async downloadVideo(@Param('videoLink') videoLink: string, @Res() res: Response) {
+    try {
+      // Extraer el identificador del video de YouTube desde el enlace completo
+      const videoId = this.extractVideoIdFromLink(videoLink);
+
+      const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+      const info = await ytdl.getInfo(videoUrl);
+      
+      // Filtrar los formatos disponibles para elegir uno que tenga audio y calidad más alta
+      const videoFormat = ytdl.chooseFormat(info.formats, { quality: 'highest' , filter: 'audioandvideo' });
+
+      res.set({
+        'Content-Disposition': `attachment; filename="${info.videoDetails.title}.mp4"`,
+        'Content-Type': 'video/mp4',
+      });
+
+      ytdl(videoUrl, { format: videoFormat }).pipe(res);
+    } catch (error) {
+      console.error('Error downloading video:', error);
+      res.status(500).send('Error downloading video.');
+    }
+  }
+
+  private extractVideoIdFromLink(videoLink: string): string {
+    // Puedes usar expresiones regulares u otros métodos para extraer el identificador del video
+    // Aquí utilizamos un ejemplo simple que asume que el identificador está al final de la URL
+    const parts = videoLink.split('?v=');
+    if (parts.length === 2) {
+      return parts[1];
+    } else {
+      throw new Error('Invalid video link');
+    }
+  }
+}
